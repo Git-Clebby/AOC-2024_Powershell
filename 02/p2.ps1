bcff1e20638ab7fff591bd8677d33fa53f11eaa2
Git-Clebby <cajasa01pps@gmail.com>
2024-12-29 15:36:35 -0600

    2nd commit
    had to fix local repo

---
+++ 02/p2.ps1
@@ -0,0 +1,60 @@
+$numbers = Get-Content -Path '.\input.txt'
+ 
+function Test-Sequence {
+  param (
+    [int[]]$sequence
+  )
+    
+  $direction = $null
+    
+  for ($i = 0; $i -lt $sequence.Length - 1; $i++) {
+    $diff = $sequence[$i + 1] - $sequence[$i]
+    $safeDist = [Math]::Abs($diff)
+        
+    if ($safeDist -lt 1 -or $safeDist -gt 3) {
+      return $false
+    }
+        
+    if ($null -eq $direction) {
+      $direction = $diff -gt 0
+    } elseif (($diff -gt 0) -ne $direction) {
+      return $false
+    }
+  }
+  return $true
+}
+ 
+function Test-Row {
+  param (
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string]$row
+  )
+    
+  # Convert string to int array
+  $nums = $row -split ' ' | ForEach-Object { [int]$_ }
+    
+  # First check if the sequence is safe without any removals
+  if (Test-Sequence $nums) {
+    return $true
+  }
+    
+  # If not safe, try removing each number once (Problem Dampener)
+  for ($i = 0; $i -lt $nums.Length; $i++) {
+    $dampened = [System.Collections.ArrayList]@($nums)
+    $dampened.RemoveAt($i)
+        
+    if (Test-Sequence $dampened) {
+      return $true
+    }
+  }
+    
+  return $false
+}
+ 
+$safeReports = 0
+foreach ($row in $numbers) {
+  if (Test-Row $row) {
+    $safeReports++
+  }
+}
+Write-Host $safeReports
