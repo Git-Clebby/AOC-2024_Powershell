991cd8e1e1420bb9d96645abd4e96fb136b3d361
Git-Clebby <cajasa01pps@gmail.com>
2025-01-01 15:57:59 -0600

    woops

--- 03/pt1.ps1
+++ 03/pt1.ps1
@@ -4,8 +4,6 @@ $valid = [regex]::Matches($in, $pattern)
 $results = 0
 
 foreach ($match in $valid) {
-  $other = $match.Groups[0].Value
-  Write-Host $other
   $left = [int]$match.Groups[1].Value
   $right = [int]$match.Groups[2].Value
   $result = $left * $right
