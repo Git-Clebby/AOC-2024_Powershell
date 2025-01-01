f9864b890864891661d860ab5f8126f42339bb10
Git-Clebby <cajasa01pps@gmail.com>
2025-01-01 15:56:52 -0600

    removed junk

--- 03/pt1.ps1
+++ 03/pt1.ps1
@@ -1,11 +1,9 @@
 $in = Get-Content -Path '.\input.txt'
-#$pattern = "mul\(\d+,\d+\)"
-#$matchers = ($in | Select-String -Pattern $pattern -AllMatches).Matches.Value
 $pattern = "mul\((\d+),(\d+)\)"
-$matchers = [regex]::Matches($in, $pattern)
+$valid = [regex]::Matches($in, $pattern)
 $results = 0
 
-foreach ($match in $matchers) {
+foreach ($match in $valid) {
   $other = $match.Groups[0].Value
   Write-Host $other
   $left = [int]$match.Groups[1].Value
